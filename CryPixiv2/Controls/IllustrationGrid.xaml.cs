4071a60ec4b4351852176c952f04e28c449e0cbf
Adam Veselič <adam.veselic@hotmail.com>
2018-05-08 19:15:17 +0200

    Some slight changes

--- CryPixiv2/Controls/IllustrationGrid.xaml.cs
+++ CryPixiv2/Controls/IllustrationGrid.xaml.cs
@@ -33,7 +33,6 @@ namespace CryPixiv2.Controls
         }
 
 
-
         #region Animations
         private void GridView_Loaded(object sender, RoutedEventArgs e)
         {
@@ -52,7 +51,7 @@ namespace CryPixiv2.Controls
             };
             mylist.IsItemClickEnabled = true;
         }
-        private int animationIndex = 0;
+
         private void InitializeAnimations(Panel panel)
         {
             var compositor = ElementCompositionPreview.GetElementVisual(mylist).Compositor;
@@ -64,29 +63,27 @@ namespace CryPixiv2.Controls
 
             // item animations
             mylist.LayoutUpdated += (a, b) =>
-            {
-                for (int i = animationIndex; i < ItemSource.Collection.Count; i++)
+            {               
+                // try to optimize this
+                for (int i = 0; i < ItemSource.Collection.Count; i++)
                 {
                     var itemContainer = (GridViewItem)mylist.ContainerFromItem(ItemSource.Collection[i]);
-                    if (itemContainer == null)
-                    {
-                        animationIndex = i;
-                        break;
-                    }
-
+                    if (itemContainer == null) break;
+                    
                     var visual = ElementCompositionPreview.GetElementVisual(itemContainer);
                     if (visual.ImplicitAnimations != null) continue;
                     visual.ImplicitAnimations = elementImplicitAnimation;
                 }
             };
         }
+        private void ItemsWrapGrid_Loaded(object sender, RoutedEventArgs e) => InitializeAnimations((Panel)sender);
         private static CompositionAnimationGroup CreateOffsetAnimation(Compositor compositor)
         {
             // Define Offset Animation for the Animation group
             var offsetAnimation = compositor.CreateVector3KeyFrameAnimation();
             offsetAnimation.InsertExpressionKeyFrame(1, "this.FinalValue");
             offsetAnimation.Duration = TimeSpan.FromSeconds(.8);
-
+            
             // Define Animation Target for this animation to animate using definition. 
             offsetAnimation.Target = "Offset";
 
@@ -97,7 +94,5 @@ namespace CryPixiv2.Controls
             return animationGroup;
         }
         #endregion
-
-        private void ItemsWrapGrid_Loaded(object sender, RoutedEventArgs e) => InitializeAnimations((Panel)sender);      
     }
 }
